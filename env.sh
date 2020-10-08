b249fd830b81d562ef6dfdea32cad374fd9de31d
Munieru <20086673+munierujp@users.noreply.github.com>
2020-10-08 13:56:50 +0900

    Create env.sh

---
+++ env.sh
@@ -0,0 +1,28 @@
+# macOS
+MAC_VERSION=$(sw_vers -productVersion)
+MAC_BUILD_VERSION=$(sw_vers -buildVersion)
+echo "macOS v${MAC_VERSION} (${MAC_BUILD_VERSION})"
+
+# zsh
+ZSH_VERSION=$(zsh --version)
+echo $ZSH_VERSION
+
+# Homebrew
+HOMEBREW_VERSION=$(brew --version)
+echo $HOMEBREW_VERSION
+
+# Git
+GIT_VERSION=$(git --version)
+echo $GIT_VERSION
+
+# Node.js
+NODE_VERSION=$(node --version)
+echo "node ${NODE_VERSION}"
+
+# npm
+NPM_VERSION=$(npm --version)
+echo "npm v${NPM_VERSION}"
+
+# yarn
+YARN_VERSION=$(yarn --version)
+echo "yarn v${YARN_VERSION}"
