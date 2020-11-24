ae50a4c4c72e98e90cc3b167888f717805fb84c4
Munieru <20086673+munierujp@users.noreply.github.com>
2020-11-24 18:03:17 +0900

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,7 +1,25 @@
+echo "# Hardware"
+
+MAC_HARDWARE_INFO=$(system_profiler -json SPHardwareDataType | jq '.SPHardwareDataType[0]')
+MAC_MACHINE_NAME=$(echo $MAC_HARDWARE_INFO | jq --raw-output '.machine_name')
+MAC_MACHINE_MODEL=$(echo $MAC_HARDWARE_INFO | jq --raw-output '.machine_model')
+echo "Model: ${MAC_MACHINE_NAME} (${MAC_MACHINE_MODEL})"
+
+# CPU
+MAC_CPU_TYPE=$(echo $MAC_HARDWARE_INFO | jq --raw-output '.cpu_type')
+MAC_CPU_SPEED=$(echo $MAC_HARDWARE_INFO | jq --raw-output '.current_processor_speed')
+echo "CPU: ${MAC_CPU_TYPE} ${MAC_CPU_SPEED}"
+
+# Memory
+MAC_MEMORY=$(echo $MAC_HARDWARE_INFO | jq --raw-output '.physical_memory')
+echo "Memory: ${MAC_MEMORY}"
+
+echo "# Softwares"
+
 # macOS
-MAC_VERSION=$(sw_vers -productVersion)
-MAC_BUILD_VERSION=$(sw_vers -buildVersion)
-echo "macOS v${MAC_VERSION} (${MAC_BUILD_VERSION})"
+MAC_SOFTWARE_INFO=$(system_profiler -json SPSoftwareDataType | jq '.SPSoftwareDataType[0]')
+MAC_OS=$(echo $MAC_SOFTWARE_INFO | jq --raw-output '.os_version')
+echo $MAC_OS
 
 # zsh
 ZSH_VERSION=$(zsh --version)
